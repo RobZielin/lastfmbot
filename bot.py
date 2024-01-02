a7506f7832ba8f5cca49b9877fb838a3a58b5f80
Rob <61631842+RobZielin@users.noreply.github.com>
2024-01-02 13:23:37 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 import actions
 
-
+#parses user messages in actions.py
 async def event(message, user_message, privacy):
     try:
         response = await actions.get_response(message, user_message)
@@ -10,7 +10,7 @@ async def event(message, user_message, privacy):
     except Exception as e:
         print(e)
 
-
+#initializes the bot, token must be pasted in token.txt
 def run():
     with open("token.txt", "r") as file:
         token = file.read()
@@ -18,10 +18,12 @@ def run():
     intents.message_content = True
     client = discord.Client(intents=intents)
 
+    #upon bot login print success message
     @client.event
     async def on_ready():
         print(f'{client.user} successfully logged in')
 
+    #listen for user messages
     @client.event
     async def on_message(message):
         if message.author == client.user:
@@ -33,8 +35,10 @@ def run():
 
         print(f'{username} said: "{user_message}" ({channel})')
 
+        #if message begins with the correct prefix '!', parse it
         if message.content.startswith("!"):
             user_message = message.content[1:].strip()
             await event(message, user_message, privacy=message.channel.type == 'private')
 
+    #runs the client
     client.run(token)
