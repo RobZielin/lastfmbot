021fde3eece4c6575a7a8b4f6b9668bed4c96f4d
Rob <61631842+RobZielin@users.noreply.github.com>
2024-01-02 13:24:25 -0500

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,5 +1,6 @@
 import discord
 
+#if the user message matches an expected input, return the response
 async def get_response(message: discord.Message, user_message: str) -> str:
     p_message = user_message.lower()
 
